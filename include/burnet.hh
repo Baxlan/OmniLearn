25819b8e6b56674176c5334ba2b959bc0476fada
Baxlan <cavalioz@hotmail.fr>
2019-06-20 23:54:47 +0200

    Bias has been taken into account. Neuron has been implemented.

--- include/burnet.hh
+++ include/burnet.hh
@@ -75,6 +75,18 @@ struct AggregationException : public Exception
 };
 
 
+struct ProcessException : public Exception
+{
+    ProcessException(std::string const& msg):
+    Exception(msg)
+    {
+    }
+
+    virtual ~ProcessException()
+    {
+    }
+};
+
 
 //=============================================================================
 //=============================================================================
@@ -118,6 +130,15 @@ double distance(std::vector<double> const& a, std::vector<double> const& b, doub
 }
 
 
+double average(std::vector<double> const& a)
+{
+    double sum = 0;
+    for(double const& val : a)
+    {
+        sum += val;
+    }
+    return sum / a.size();
+}
 
 //=============================================================================
 //=============================================================================
@@ -216,7 +237,7 @@ public:
         {
             result += (_weights[0][i] * inputs[i]);
         }
-        return {dot(inputs, _weights[0]), 0};
+        return {dot(inputs, _weights[0]) + _bias[0], 0};
     }
 
 
@@ -257,7 +278,7 @@ public:
 
         for(unsigned i = 0; i < _weights[0].size(); i++)
         {
-            result[i] += (-std::pow((inputs[i] - _weights[0][i]), _order-1) * a);
+            result[i] += (-std::pow((inputs[i] - _weights[0][i]) + _bias[0], _order-1) * a);
         }
         return result;
     }
@@ -289,7 +310,7 @@ public:
 
         for(unsigned i = 0; i < _k; i++)
         {
-            dots[i] = dot(inputs, _weights[i]);
+            dots[i] = dot(inputs, _weights[i]) + _bias[i];
         }
 
         std::pair<double, unsigned> max = {dots[0], 0};
@@ -531,7 +552,7 @@ protected:
 
 
 
-// S-shaped rectified linear unit (combination of 3 linear functions, with 4 learnable parameters )
+// if there are two hinges, then this is S-shaped rectified linear unit (Srelu)
 class Srelu : public Activation
 {
     void learn([[maybe_unused]] double gradient, [[maybe_unused]] double learningRate, [[maybe_unused]] double momentum)
@@ -540,9 +561,7 @@ class Srelu : public Activation
     }
 
 protected:
-    double _coef1;
-    double _coef2;
-    double _coef3;
+
 };
 
 
@@ -608,9 +627,97 @@ protected:
 
 class Neuron
 {
+    double process(std::vector<double> const& inputs)
+    {
+        return _Activation->activate(_Aggregation->aggregate(inputs).first);
+    }
+
+
+    double processToLearn(std::vector<double> const& inputs)
+    {
+        if(_currentFeature >= (_batchSize - 1))
+        {
+            throw ProcessException("Batch size have been reashed, gradients must be calculated then parameters must be updated before processing new features.");
+        }
+        _currentFeature++;
+        _aggregResults[_currentFeature-1] = _Aggregation->aggregate(inputs);
+        _activResults[_currentFeature-1] = _Activation->activate(_aggregResults[_currentFeature-1].first);
+        return _activResults[_currentFeature-1];
+    }
+
+
+    void processGradient(std::vector<double> inputGradients) //one input by feature in the batch
+    {
+        if(_currentFeature < (_batchSize - 1))
+        {
+            throw ProcessException("Calculating gradient but the batch size have not been reashed.");
+        }
+        else if(_currentFeature > (_batchSize - 1))
+        {
+            throw ProcessException("Calculating gradient but parameters have not be updated.");
+        }
+
+        std::vector<double> actGradients(_batchSize, 0); //store gradients between activation and aggregation for each feature of the batch
+
+        for(_currentFeature = 0; _currentFeature < _batchSize; _currentFeature++)
+        {
+            _inputGradients[_currentFeature] = inputGradients[_currentFeature];
+            actGradients[_currentFeature] = _inputGradients[_currentFeature] * _Activation->prime(_activResults[_currentFeature]); // * aggreg.prime ?
+            _averageActGradient = average(actGradients);
+        }
+
+        //setting all partial gradients on 0
+        _gradients = std::vector<std::vector<double>>(_Aggregation->k(), std::vector<double>(_batchSize, 0));
+
+        //storing new partial gradients
+        for(_currentFeature = 0; _currentFeature < _currentFeature; _currentFeature++)
+        {
+            _gradients[_aggregResults[_currentFeature].second][_currentFeature] = _aggregResults[_currentFeature].first * actGradients[_currentFeature];
+        }
+
+        _currentFeature++;
+    }
+
+
+    void updateWeights(double learningRate, double L1, double L2, double tackOn, double maxNorm, double momentum)
+    {
+        if(_currentFeature <= _batchSize - 1)
+        {
+            throw ProcessException("Updating parameters but gradients have not been calculated.");
+        }
+
+        _Activation->learn(average(_inputGradients), 0, 0);
+        _Aggregation->learn(_averageActGradient, 0, 0);
+
+        for(unsigned i = 0; i < _Aggregation->k(); i++)
+        {
+            std::pair<std::vector<double>&, double&> w = _Aggregation->weightRef(i);
+            double gradient = average(_gradients[i]);
+
+            for(unsigned j = 0; j < w.first.size(); j++)
+            {
+                w.first[j] += (learningRate*(gradient + (L2*w.first[j]) + L1) + tackOn);
+            }
+            w.second += learningRate * gradient; // to divide by inputs
+        }
+    }
+
 protected:
     std::shared_ptr<Activation> _Activation;   //should be a value but polymorphism is needed
     std::shared_ptr<Aggregation> _Aggregation; //should be a value but polymorphism is needed
+
+    unsigned const _batchSize;
+
+    unsigned _currentFeature; // current feature number in mini-batch
+    std::vector<std::pair<double, unsigned>> _aggregResults; // results obtained by aggregation and weight set used, for each feature in the batch
+    std::vector<double> _activResults; // results obtained by activation, for each feature in the batch
+
+    std::vector<std::vector<double>> _previousWeightUpdate; // previous update aplied to each weight in each weight set
+    std::vector<double> _previousBiasUpdate; // previous update aplied to bias for each weight set
+    std::vector<double> _inputGradients; //for each feature of the batch, gradients entered from previous layers
+    double _averageActGradient; //averaged gradient over all features of the batch, between activation and aggregation
+    std::vector<std::vector<double>> _gradients; // partial gradient obtained for each feature of the batch and for each weight set
+    //weight set //feature //partial gradient
 };
 
 
@@ -624,9 +731,59 @@ protected:
 //=============================================================================
 
 
+class Layer
+{
+public:
+    std::vector<double> process()
+    {
+
+    }
+
+    std::vector<double> processToLearn()
+    {
+
+    }
+
+    void processGradients()
+    {
+
+    }
+
+    void updateWeights()
+    {
+
+    }
+
+protected:
+    double const _dropout;
+    double const _dropconnect;
+    double const _maxNorm;
+};
 
 
 
+//=============================================================================
+//=============================================================================
+//=============================================================================
+//=== NETWORK ==================================================================
+//=============================================================================
+//=============================================================================
+//=============================================================================
+
+
+class Network
+{
+public:
+
+
+protected:
+    double _learningRate;
+    double const _L1;
+    double const _L2;
+    double const _tackOn;
+    double const _maxEpoch;
+    double _currentEpoch;
+};
 
 
 //=============================================================================
