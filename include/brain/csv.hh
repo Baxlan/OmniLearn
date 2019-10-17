bfa742eeaceafb0ad7609e04defbec845bfee5e1
Baxlan <cavalioz@hotmail.fr>
2019-10-17 23:53:56 +0200

    added brain formated csv loader (not working yet)

--- include/brain/csv.hh
+++ include/brain/csv.hh
@@ -1,9 +1,118 @@
 #ifndef BRAIN_CSV_HH_
 #define BRAIN_CSV_HH_
 
+#include <fstream>
+#include <algorithm>
+
+#include "Matrix.hh"
+
 namespace brain
 {
 
+
+
+//=============================================================================
+//=============================================================================
+//=============================================================================
+//=== DATA STRUCT =============================================================
+//=============================================================================
+//=============================================================================
+//=============================================================================
+
+
+struct Data
+{
+  Data():
+  inputs(),
+  outputs(),
+  inputLabels(0),
+  outputLabels(0)
+  {}
+
+  Matrix inputs;
+  Matrix outputs;
+  std::vector<std::string> inputLabels;
+  std::vector<std::string> outputLabels;
+};
+
+
+
+//=============================================================================
+//=============================================================================
+//=============================================================================
+//=== LOAD DATA FUNCTION ======================================================
+//=============================================================================
+//=============================================================================
+//=============================================================================
+
+
+Data loadData(std::string const& path, char separator = ';')
+{
+  std::fstream dataFile(path);
+  std::vector<std::string> content;
+  Data data;
+
+  //load data
+  while(dataFile.good())
+  {
+    content.push_back("");
+    std::getline(dataFile, content[content.size()-1]);
+    if(content[content.size()-1][content[content.size()-1].size()-1] != separator)
+      content[content.size()-1] += separator;
+  }
+
+  size_t elements = static_cast<size_t>(std::count(content[0].begin(), content[0].end(), separator));
+  size_t i = 0;
+
+  //extract inputs labels
+  std::string val;
+  for(i = 0; i < elements; i++)
+  {
+    val = content[0].substr(0, content[0].find(separator));
+    if(val[0] == separator)
+      break;
+    data.inputLabels.push_back(val);
+    content[0].erase(0, content[0].find(separator) + 1);
+  }
+  content[0].erase(0, content[0].find(separator) + 1);
+
+  //extract output labels
+  for(; i < elements; i++)
+  {
+    val = content[0].substr(0, content[0].find(separator));
+    data.outputLabels.push_back(val);
+    content[0].erase(0, content[0].find(separator) + 1);
+  }
+
+  //remove label line
+  content.erase(content.begin());
+
+  data.inputs.reserveLines(content.size());
+  data.inputs.reserveColumns(data.inputLabels.size());
+  data.outputs.reserveLines(content.size());
+  data.outputs.reserveColumns(data.outputLabels.size());
+
+  for(size_t j = 0; j < content.size(); j++)
+  {
+    val = content[j];
+    for(i = 0; i < data.inputLabels.size(); i++)
+    {
+      data.inputs[j][i] = (std::stod(val.substr(0, val.find(separator))));
+      val.erase(0, val.find(separator) + 1);
+    }
+    val.erase(0, val.find(separator) + 1);
+    for(i = 0; i < data.outputLabels.size(); i++)
+    {
+      data.outputs[j][i] = (std::stod(val.substr(0, val.find(separator))));
+      val.erase(0, val.find(separator) + 1);
+    }
+  }
+
+  return data;
 }
+
+
+
+} // namespace brain
 
 #endif // BRAIN_CSV_HH_
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "brain/brain.hh"
 #include "brain/json.hh"
 #include "brain/Matrix.hh"
+#include "brain/csv.hh"
 #include <deque>
 
 
@@ -80,6 +81,8 @@ int main()
 
     std::vector<std::string> labels = {"Mo95","Tc99","Ru101","Rh103","Ag109","Cs133","Nd143","Sm147","Sm149","Sm150","Sm151","Sm152","Gd155","Eu153","U235","U238","Pu238","Pu239","Pu240","Pu241","Pu242","Am241","Am242"};
     //std::vector<std::string> labels = {"setosa", "versicolor", "virginica"};
+
+    //brain::Data d = brain::loadData("dataset/vesta.csv");
 
     brain::NetworkParam netp;
     netp.threads = 1;
